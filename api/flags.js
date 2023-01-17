365616c44074458af44841254109d9314ebfe6d6
Ahmed K.A <ahmadkabdullah@pm.me>
2023-01-17 18:29:48 +0300

    Add :id limit

--- api/flags.js
+++ api/flags.js
@@ -4,23 +4,25 @@ import { db } from "../index.js";
 const flagsRouter = Router();
 
 flagsRouter.route("/").get(async (req, res) => {
-	res.json(
-		db.data.map((el) => {
-			delete el.answer;
-			return el;
-		}),
-	);
+	let dataWithoutAnswer = [...db.data].map((el) => {
+		delete el.answer;
+		return el;
+	});
+
+	res.json(dataWithoutAnswer);
 });
 
 flagsRouter.route("/:id").get(async (req, res) => {
 	let id = req.params.id;
 
+	// return if out of length
+	if (id > 520) return res.sendStatus(404);
+
 	// return if no answer is given
-	let answer = req.query.answer;
-	if (!answer) return res.sendStatus(400);
+	if (!req.query.answer) return res.sendStatus(400);
 
 	let correctAnswer = db.data[id].answer;
-	let userAnswer = answer;
+	let userAnswer = req.query.answer;
 	let isCorrect = correctAnswer === userAnswer;
 
 	res.json({ isCorrect, correctAnswer, userAnswer });
