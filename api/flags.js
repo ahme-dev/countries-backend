864e5d8aa9f91c3ac7d3207826441b31116437b5
Ahmed K.A <ahmadkabdullah@pm.me>
2023-01-17 17:17:18 +0300

    Prepare flags data and read db in index

--- api/flags.js
+++ api/flags.js
@@ -1,38 +1,16 @@
 import { Router } from "express";
-import { Low } from "lowdb";
-import { JSONFile } from "lowdb/node";
+import { db } from "../index.js";
 
 const flagsRouter = Router();
 
 flagsRouter.route("/").get(async (req, res) => {
-	// config lowdb
-	const file = "data/flags.json";
-	const adapter = new JSONFile(file);
-	const db = new Low(adapter);
+	res.json(db.data);
+});
 
-	await db.read();
+flagsRouter.route("/:id").get(async (req, res) => {
+	let id = req.params.id;
 
-	if (db.data) {
-		res.send("Already have data");
-	}
-
-	const fetchRes = await fetch(
-		"https://shadify.dev/api/countries/country-quiz?amount=20",
-	);
-	// get data
-	const fetchData = await fetchRes.json();
-	console.log("datais ", fetchData);
-	// fix flag sizes
-	const fetchDataFixed = fetchData.map((el) => ({
-		...el,
-		flag: el.flag.replace("w320", "256x192"),
-	}));
-
-	db.data = fetchDataFixed;
-
-	await db.write();
-
-	res.send("Flags Hello");
+	res.json(db.data[id]);
 });
 
 export { flagsRouter };
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
 import express, { json } from "express";
 import { flagsRouter } from "./api/flags.js";
 const app = express();
+
+// config and read lowdb
+const file = "data/flags.json";
+const adapter = new JSONFile(file);
+export const db = new Low(adapter);
+await db.read();
 
 // middleware
 app.use(json());
