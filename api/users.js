fad698ee43d97d0afe9088657f1444bdd9315d87
Ahmed K.A <ahmadkabdullah@pm.me>
2023-01-20 17:53:15 +0300

    Add route for either flags or capitals to add answers and increase index

--- api/users.js
+++ api/users.js
@@ -41,36 +41,51 @@ usersRouter
 		res.sendStatus(201);
 	});
 
-usersRouter
-	.route("/:username")
-	.get(async (req, res) => {
-		// find user
-		let user = usersDB.data.find((el) => el.username === req.params.username);
+usersRouter.route("/:username").get(async (req, res) => {
+	// find user
+	let user = usersDB.data.find((el) => el.username === req.params.username);
 
-		// return if user not found
-		if (!user) return res.sendStatus(404);
+	// return if user not found
+	if (!user) return res.sendStatus(404);
 
-		// make clone without password
-		let userWithoutPass = JSON.parse(JSON.stringify(user));
-		delete userWithoutPass.password;
+	// make clone without password
+	let userWithoutPass = JSON.parse(JSON.stringify(user));
+	delete userWithoutPass.password;
 
-		res.json(userWithoutPass);
-	})
+	res.json(userWithoutPass);
+});
 
-	.patch(async (req, res) => {
-		// try to find user index
-		let userID = usersDB.data.findIndex(
-			(el) => el.username === req.params.username,
-		);
+usersRouter.route("/:username/:type").patch(async (req, res) => {
+	let answerType = req.params.type;
+	console.log(answerType);
 
-		// return if user not found
-		if (userID === -1) return res.sendStatus(404);
+	// return if not on either route
+	if (answerType !== "flags" && answerType !== "capitals") {
+		return res.sendStatus(404);
+	}
 
-		// push result to users results
-		usersDB.data[userID].results.push(req.body.result);
-		await usersDB.write();
+	// try to find user index
+	let userID = usersDB.data.findIndex(
+		(el) => el.username === req.params.username,
+	);
 
-		res.sendStatus(202);
-	});
+	// return if user not found
+	if (userID === -1) return res.sendStatus(404);
+
+	// push answer to users results based on type
+	if (answerType === "flags") {
+		usersDB.data[userID].flags.answers.push(req.body.answer);
+		usersDB.data[userID].flags.index += 1;
+	}
+	if (answerType === "capitals") {
+		usersDB.data[userID].capitals.answers.push(req.body.answer);
+		usersDB.data[userID].capitals.index += 1;
+	}
+
+	// increase user index
+	await usersDB.write();
+
+	res.sendStatus(202);
+});
 
 export { usersRouter };
