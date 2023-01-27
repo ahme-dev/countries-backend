3008d4b600bee83249df4634af8f935849b7f79c
Ahmed K.A <ahmadkabdullah@pm.me>
2023-01-27 20:01:29 +0300

    Add register route and add many comments

--- routes/auth.js
+++ routes/auth.js
@@ -9,14 +9,51 @@ await usersDB.read();
 export const authRouter = Router();
 
 authRouter.post("/login", (req, res) => {
+	// if no username and password provided return bad request
+	if (!req.body.username || req.body.password) return res.sendStatus(400);
+
+	// try to find user
 	let user = usersDB.data.find((el) => el.username === req.body.username);
 
-	if (!user) return res.sendStatus(401);
+	// if user not found return not found
+	if (!user) return res.sendStatus(404);
 
-	if (user.password !== req.body.password) return res.sendStatus(401);
+	// if password is wrong return not found
+	if (user.password !== req.body.password) return res.sendStatus(404);
 
-	// save username in session
+	// otherwise save username in session
 	req.session.username = user.username;
+
+	return res.sendStatus(200);
+});
+
+authRouter.post("/register", async (req, res) => {
+	// if no username and password provided return bad request
+	if (!req.body.username || req.body.password) return res.sendStatus(400);
+
+	// try to find user with same username
+	let user = usersDB.data.find((el) => el.username === req.body.username);
+
+	// if user found return bad request
+	if (user) return res.sendStatus(400);
+
+	// create new user from provided values
+	let newUser = {
+		username: req.body.username,
+		password: req.body.password,
+		flags: {
+			index: 0,
+			answers: [],
+		},
+		capitals: {
+			index: 0,
+			answers: [],
+		},
+	};
+
+	// push new user into db and write
+	usersDB.data.push(newUser);
+	await usersDB.write();
 
 	return res.sendStatus(200);
 });
