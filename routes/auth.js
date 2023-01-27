c8da007c5fc5c372441fecdc2a7d2c7d4fce09cf
Ahmed K.A <ahmadkabdullah@pm.me>
2023-01-27 20:51:38 +0300

    Extend console logs to find problems

--- routes/auth.js
+++ routes/auth.js
@@ -9,19 +9,23 @@ await usersDB.read();
 export const authRouter = Router();
 
 authRouter.post("/login", (req, res) => {
-	console.log("login: request body", req.body);
+	console.log("login: start request body", req.body);
+
 	// if no username and password provided return bad request
 	if (!req.body.username || !req.body.password) return res.sendStatus(400);
 
+	console.log("login: will try to find user in", usersDB.data);
+
 	// try to find user
 	let user = usersDB.data.find((el) => el.username === req.body.username);
-
-	console.log("login: before finding user");
-
 	// if user not found return not found
 	if (!user) return res.sendStatus(404);
 
-	console.log("login: before checking password");
+	console.log(
+		"login: will try to compare passwords",
+		user.password,
+		req.body.password,
+	);
 
 	// if password is wrong return not found
 	if (user.password !== req.body.password) return res.sendStatus(404);
@@ -29,21 +33,25 @@ authRouter.post("/login", (req, res) => {
 	// otherwise save username in session
 	req.session.username = user.username;
 
+	console.log("login: done and session added");
+
 	return res.sendStatus(200);
 });
 
 authRouter.post("/register", async (req, res) => {
-	console.log("register: request body", req.body);
+	console.log("register: start request body", req.body);
+
 	// if no username and password provided return bad request
 	if (!req.body.username || !req.body.password) return res.sendStatus(400);
 
+	console.log("register: will try to find user in", usersDB.data);
+
 	// try to find user with same username
 	let user = usersDB.data.find((el) => el.username === req.body.username);
-
-	console.log("register: before finding user. List is", usersDB.data);
-
 	// if user found return bad request
 	if (user) return res.sendStatus(400);
+
+	console.log("register: good, no other user by that name");
 
 	// create new user from provided values
 	let newUser = {
@@ -62,6 +70,8 @@ authRouter.post("/register", async (req, res) => {
 	// push new user into db and write
 	usersDB.data.push(newUser);
 	await usersDB.write();
+
+	console.log("register: done and written to db");
 
 	return res.sendStatus(200);
 });
